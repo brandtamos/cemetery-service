b933d606d9694adf1096546a81139f023d34bc14
596187 <evanbrandt@c02vt0m5htdg.users.bah.com>
2018-02-26 12:56:13 -0500

    Testing returning a JSON object

--- index.js
+++ index.js
@@ -2,13 +2,46 @@ var app = require('express')();
 const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+ 	res.send('Hello World!');
+});
+
+app.get('/cemetary/schedule/1', function (req, res) {
+	var schedule = {
+					  "CemetaryName": "XYZ National Cemetary",
+					  "CemetaryInstructions": "Make sure everything is correct",
+					  "SchedulingInfo": {
+					    "SchedulingPeriodUnitMinutes": "15",
+					    "Schedule": [
+					      {
+					        "PeriodTime": "09:00:00.000",
+					        "RemaingSlots": "2"
+					      },
+					      {
+					        "PeriodTime": "09:15:00.000",
+					        "RemaingSlots": "1"
+					      },
+					      {
+					        "PeriodTime": "09:30:00.000",
+					        "RemaingSlots": "2"
+					      },
+					      {
+					        "PeriodTime": "09:45:00.000",
+					        "RemaingSlots": "0"
+					      },
+					      {
+					        "PeriodTime": "10:00:00.000",
+					        "RemaingSlots": "2"
+					      }
+					    ]
+					  }
+					};
+ 	res.send(schedule);
 });
  
 var server = app.listen(PORT, function () {
-  var host = server.address().address;
-  host = (host === '::' ? 'localhost' : host);
-  var port = server.address().port;
- 
-  console.log('listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	host = (host === '::' ? 'localhost' : host);
+	var port = server.address().port;
+
+	console.log('listening at http://%s:%s', host, port);
 });
