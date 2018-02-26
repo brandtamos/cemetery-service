c9901ea9f0a87419049f857d2692dd7545909271
596187 <evanbrandt@c02vt0m5htdg.users.bah.com>
2018-02-26 12:49:29 -0500

    Yep

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var app = require('express')();
- 
+const PORT = process.env.PORT || 3000
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
  
-var server = app.listen(3000, function () {
+var server = app.listen(PORT, function () {
   var host = server.address().address;
   host = (host === '::' ? 'localhost' : host);
   var port = server.address().port;
