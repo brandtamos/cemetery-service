3ec1856517f9efe27b8394a6caa82b0e90077c20
596187 <evanbrandt@C02VT0M5HTDG.local>
2018-03-04 17:23:51 -0500

    Setting timezone offset

--- schedule.js
+++ schedule.js
@@ -3,10 +3,12 @@ var moment = require('moment');
 
 var scheduler  = function () {
     var self = this;
-    this.getAvailability = function(startDate, endDate){
-        var currentTime = moment(startDate * 1000).utcOffset('-0500').startOf('day').add(9, 'hours');
+    var offset = '-500';
 
-        var endTime = moment(endDate * 1000).utcOffset('-0500').startOf('day').add(17, 'hours');
+    this.getAvailability = function(startDate, endDate){
+        var currentTime = moment(startDate * 1000).utcOffset(offset).startOf('day').add(9, 'hours');
+
+        var endTime = moment(endDate * 1000).utcOffset(offset).startOf('day').add(17, 'hours');
 
         var timeSlots = [];
         while(currentTime < endTime){
@@ -19,7 +21,7 @@ var scheduler  = function () {
             currentTime.add(1, 'hours');
 
             if(currentTime.get('hour') >= 17){
-                currentTime.add(1, 'days').startOf('day').add(9, 'hours');
+                currentTime.add(1, 'days').utcOffset(offset).startOf('day').add(9, 'hours');
             }
         }
 
