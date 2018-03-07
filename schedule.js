b4b4a119979386c55303f8cb6c7c51d209e64154
596187 <evanbrandt@c02vt0m5htdg.users.bah.com>
2018-03-07 13:45:20 -0500

    More DST bugs

--- schedule.js
+++ schedule.js
@@ -3,7 +3,6 @@ var moment = require('moment-timezone');
 
 var scheduler  = function () {
     var self = this;
-    var offset = '-0500';
     var timeZone = 'America/New_York';
 
     this.getAvailability = function(startDate, endDate){
@@ -11,17 +10,23 @@ var scheduler  = function () {
         //var endTime = moment(endDate * 1000).utcOffset(offset).startOf('day').add(17, 'hours');
 
         var currentTime = moment(startDate * 1000).tz(timeZone).startOf('day').add(9, 'hours');
+        //gross DST transition hack
+        if(currentTime.get('hour') == 10){
+            currentTime.add(-1, 'hours');
+        }
+
         var endTime = moment(endDate * 1000).tz(timeZone).startOf('day').add(17, 'hours');
         var isDST = currentTime.isDST();
         var timeSlots = [];
         while(currentTime < endTime){
             if(currentTime.isDST() != isDST) {
-                if(!currentTime.isDST()) currentTime.add(1, 'hours');
-                else currentTime.add(-1, 'hours');
+                if(!currentTime.isDST()) currentTime.add(-1, 'hours');
+                //else currentTime.add(1, 'hours');
             }
             isDST = currentTime.isDST();
             var timeslot = {
-                "time": currentTime.tz(timeZone).unix(),
+                //"time": currentTime.tz(timeZone).unix(),
+                "time": currentTime.tz(timeZone).toString(),
                 "status": Math.floor(Math.random() * 10) % 2 == 0 ? "AVAILABLE" : "UNAVAILABLE"
             }
             timeSlots.push(timeslot);
